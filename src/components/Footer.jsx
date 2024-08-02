91f34cdf2fac18bb243ec96fa20c0e65e1bbb740
Thakur Neupane <thakur.neupane.neupane@gmail.com>
2024-08-02 21:25:45 +1000

    finished with the front ui

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+const Footer = () => {
+  return (
+    <>
+      <div className="copyright">
+        &copy; CopyRight 2024. All Rights Reserved By Thakur Neupane
+      </div>
+    </>
+  );
+};
+
+export default Footer;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+import "./Navbar.css"; // Ensure you have a CSS file to include the styles
+
+const Navbar = () => {
+  const [show, setShow] = useState(false);
+  const { isAuthenticated } = useSelector((state) => state.user);
+
+  return (
+    <>
+      <nav className={show ? "navbar show_navbar" : "navbar"}>
+        <div className="logo">
+          <div className="logo-text">
+            <h1>Tech Career</h1>
+            <p className="slogan">Connect, Discover, Succeed</p>
+          </div>
+        </div>
+        <div className="links">
+          <ul>
+            <li>
+              <Link to={"/"} onClick={() => setShow(!show)}>
+                HOME
+              </Link>
+            </li>
+            <li>
+              <Link to={"/jobs"} onClick={() => setShow(!show)}>
+                JOBS
+              </Link>
+            </li>
+            {isAuthenticated ? (
+              <li>
+                <Link to={"/dashboard"} onClick={() => setShow(!show)}>
+                  DASHBOARD
+                </Link>
+              </li>
+            ) : (
+              <li>
+                <Link to={"/login"} onClick={() => setShow(!show)}>
+                  LOGIN
+                </Link>
+              </li>
+            )}
+          </ul>
+        </div>
+        <GiHamburgerMenu className="hamburger" onClick={() => setShow(!show)} />
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
